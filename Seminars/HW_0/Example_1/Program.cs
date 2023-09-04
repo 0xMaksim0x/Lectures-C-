604e35b17ebdb7249d479677f38fbd283026c6b1
terminator <mikkhailesku@mail.com>
2023-09-04 13:38:40 +0300

    Example_1

--- Seminars/HW_0/Example_1/Program.cs
+++ Seminars/HW_0/Example_1/Program.cs
@@ -1,1 +1,19 @@
 ﻿
+Console.WriteLine("Enter number : ");
+string strNum = Console.ReadLine();
+int num1 = int.Parse(strNum);
+
+Console.WriteLine("Enter number : ");
+strNum = Console.ReadLine();
+int num2 = int.Parse(strNum);
+
+int sqr = num2 * num2;
+
+if (sqr == num1)
+{
+System.Console.WriteLine("Да");
+}
+else
+{
+    System.Console.WriteLine("Нет");
+}
