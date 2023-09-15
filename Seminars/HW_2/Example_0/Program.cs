5cfb9446366001a756d24e85c29edfb41f7c14ae
terminator <mikkhailesku@mail.com>
2023-09-15 16:38:22 +0300

    Task_2

---
+++ Seminars/HW_2/Example_0/Program.cs
@@ -0,0 +1,41 @@
+﻿//Напишите программу, которая принимает на вход
+//координаты точки (X и Y), причём X ≠ 0 и Y ≠ 0 и выдаёт
+//номер четверти плоскости, в которой находится эта
+//точка.
+
+int GetNumber(string message)
+{
+    Console.Write($"Enter number {message} : ");
+    int num = Convert.ToInt32(Console.ReadLine());
+    return num;
+}
+
+void Show(int x, int y)
+
+{
+    if (x > 0 && y > 0)
+    {
+        System.Console.WriteLine("1 четверть");
+    }
+    else if (x < 0 && y > 0)
+    {
+        System.Console.WriteLine("2 четверть");
+    }
+    else if (x < 0 && y < 0)
+    {
+        System.Console.WriteLine("3 четверть");
+    }
+    else if (x > 0 && y < 0)
+    {
+        System.Console.WriteLine("4 четверть");
+    }
+    else if (x == 0 || y == 0)
+    {
+        Console.WriteLine("X ≠ 0 и Y ≠ 0 ");
+    }
+}
+
+int numX = GetNumber("X");
+int numY = GetNumber("Y");
+
+Show(numX, numY);
--- Seminars/HW_2/Example_0/obj/Debug/net6.0/Example_0.AssemblyInfo.cs
+++ Seminars/HW_2/Example_0/obj/Debug/net6.0/Example_0.AssemblyInfo.cs
@@ -10,12 +10,12 @@
 using System;
 using System.Reflection;
 
-[assembly: System.Reflection.AssemblyCompanyAttribute("Task_3")]
+[assembly: System.Reflection.AssemblyCompanyAttribute("Example_0")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
-[assembly: System.Reflection.AssemblyProductAttribute("Task_3")]
-[assembly: System.Reflection.AssemblyTitleAttribute("Task_3")]
+[assembly: System.Reflection.AssemblyProductAttribute("Example_0")]
+[assembly: System.Reflection.AssemblyTitleAttribute("Example_0")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
 
 // Создано классом WriteCodeFragment MSBuild.
