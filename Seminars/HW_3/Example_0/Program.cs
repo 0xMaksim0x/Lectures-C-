39f38bca485d82b830df7b91c7721540df7acdd3
terminator <mikkhailesku@mail.com>
2023-09-16 23:03:14 +0300

    Start

---
+++ Seminars/HW_3/Example_0/Program.cs
@@ -0,0 +1,20 @@
+﻿int GetNumber(string message)
+{
+    Console.Write($"Enter number {message} : ");
+    int num = Convert.ToInt32(Console.ReadLine());
+    return num;
+}
+
+int GetSum(int num)
+{
+    int sum = 0;
+    fot (int i = 1; i <= num; i++)
+    {
+        sum += i;
+    }
+    return sum;
+}
+
+int numA = GetNumber("A");
+int sum = GetSum(numA);
+System.Console.WriteLine($"sum = {sum}");
